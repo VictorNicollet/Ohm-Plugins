1e0f20efa7e269f316123239db2151da8ea22d10
Victor Nicollet <victor@nicollet.net>
2012-08-24 20:00:52 +0200

    ohmBootstrap plugin

---
+++ ohmBootstrap/ohmBootstrap.ml
@@ -0,0 +1,8 @@
+(* Ohm is © 2012 Victor Nicollet *)
+
+open BatPervasives
+open Ohm
+open Ohm.Universal
+
+let js  = "ohmBootstrap/bootstrap.js"
+
---
+++ ohmBootstrap/ohmBootstrap.mli
@@ -0,0 +1,13 @@
+(* Ohm is © 2012 Victor Nicollet 
+
+   Bootstrap code is licensed independently, under Apache License v2.0, 
+   by @twitter, @mdo and @fat.
+
+   Bootstrap image assets are CC BY 3.0 from Glyphicons free.
+*)
+
+(** Use Twitter's Bootstrap in Ohm applications.*)
+
+(** The path to bootstrap.js *)
+val js : string
+
