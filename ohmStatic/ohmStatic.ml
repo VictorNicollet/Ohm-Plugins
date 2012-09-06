d162544e973b8087b8c5fe6311277d6c01ee3f61
Victor Nicollet <victor@nicollet.net>
2012-09-06 17:21:23 +0200

    New site format

--- ohmStatic/ohmStatic.ml
+++ ohmStatic/ohmStatic.ml
@@ -1,7 +1,53 @@
 (* Ohm is © 2012 Victor Nicollet *)
 
+open BatPervasives
+open Ohm
+open Ohm.Universal
+
 type key = string
 type renaming = key -> string
-type item = [ `Page of (renaming -> Ohm.Html.writer)
-	    | `File of string ] 
+type page = <
+  body  : renaming -> Ohm.Html.writer ;
+  css   : renaming -> string list ;
+  js    : renaming -> string list ;
+  head  : renaming -> string ;
+  bcls  : string list ;
+  title : string option ;
+>
+type item = [ `Page of page | `File ] 
 type site = (string,item) BatPMap.t
+
+let export ?(rename=identity) ?(render=Html.print_page_ctx) ?(public="/public/") ~server ~title site = 
+
+  let endpoints, definitions = 
+    BatPMap.foldi begin fun key item (endpoints, definitions) -> 
+      match item with `File -> (endpoints,definitions) | `Page page ->
+	let endpoint, define = Action.declare server (rename key) Action.Args.none in
+	BatPMap.add key endpoint endpoints, 
+	(define,page) :: definitions
+    end site (BatPMap.empty, [])
+  in
+
+  let url server key = 
+    try Action.url (BatPMap.find key endpoints) server () 
+    with Not_found -> public ^ key 
+  in
+
+  List.iter begin fun (define,page) ->
+
+    define begin fun req res ->
+
+      let rename = url (req # server) in 
+      let body   = page # body  rename in
+      let css    = page # css   rename in
+      let js     = page # js    rename in
+      let head   = page # head  rename in 
+      let bcls   = page # bcls in
+      let title  = BatOption.default title (page # title) in
+
+      let! page = ohm $ render ~css ~js ~head ~body_classes:bcls ~title body in
+      return $ Action.page page res 
+
+    end 
+  end definitions
+
--- ohmStatic/ohmStatic.mli
+++ ohmStatic/ohmStatic.mli
@@ -16,13 +16,43 @@ type key = string
 *)
 type renaming = key -> string
 
+(** The type of a page. This determines what the title of the page is, what javascript 
+    and CSS files are to be included, and so on. 
+*)
+type page = <
+  body  : renaming -> Ohm.Html.writer ;
+  css   : renaming -> string list ;
+  js    : renaming -> string list ;
+  head  : renaming -> string ;
+  bcls  : string list ;
+  title : string option ;
+>
+
 (** The type of a static. This is either a bit of HTML, or a standalone file that
     can become downloadable.
 *)
-type item = [ `Page of (renaming -> Ohm.Html.writer)
-	    | `File of string ] 
+type item = [ `Page of page | `File ] 
 
 (** The type of a static site - it maps the key (which is the relative path of an 
     item within the /static directory) to its contents. 
 *)
 type site = (string,item) BatPMap.t
+
+(** Export a static site. 
+    @param rename A function that provides the path of each item. By default, 
+    the item's key is used as its path. 
+    @param server The server on which the site should run. 
+    @param title The default title to be used, if no title is provided by the page. 
+    @param render The function which is used to render the item. By default, 
+    this is the vanilla [Ohm.Html.print_page]. 
+    @param public The url prefix for files that are available for public download. 
+    By default, this is ["/public"] and points to the [www/public] directory.
+*)
+val export : 
+     ?rename:renaming
+  -> ?render:(unit Ohm.Html.ctxrenderer)
+  -> ?public:string
+  ->  server:('s Ohm.Action.server)
+  ->  title:string
+  ->  site
+  ->  unit
